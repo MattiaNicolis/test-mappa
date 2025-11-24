dcd905948c9fde4535d6837c666896df8d5e0ecf
Mattia <mattia.nicolis04@gmail.com>
2025-11-24 12:23:20 +0100

    creato home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,46 @@
+import { useState, useRef, useEffect } from "react"
+import { Mappa } from "../components/Mappa"
+
+type coloriStanzeType = {
+    id: number,
+    color: string
+}
+
+function HomePage() {
+
+    const mappaRef = useRef<SVGSVGElement>(null)
+
+    const [stanza, setStanza] = useState<SVGSVGElement[]>([])
+
+    const coloriStanza = useRef<coloriStanzeType[]>([])
+
+
+    const cambiaColore = (i: number, color:string) => {
+        stanza[i].setAttribute("fill", color)
+    }
+
+    useEffect(() => {
+        if(mappaRef.current)
+            setStanza(Array.from(mappaRef.current.querySelectorAll('[stroke="red"]')))
+    }, [])
+
+    useEffect(() => {
+        coloriStanza.current = stanza.map((_, i) => ({
+            id: i,
+            color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+        }))
+    }, [stanza])
+
+    return(
+        <div>
+            {stanza.map((_, i) => (
+                <button key={i} onClick={() => cambiaColore(i, coloriStanza.current[i].color)}> 
+                    Colora stanza {i+1}
+                </button>
+            ))}
+            <Mappa ref={mappaRef} />
+        </div>
+    )
+}
+
+export default HomePage
